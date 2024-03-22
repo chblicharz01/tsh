aa83d1fec2589058e7b5c967af6377b8e00c2858
chblicharz01 <chblicharz01@gmail.com>
2024-03-22 13:47:30 -0500

    history function added

--- tsh.c
+++ tsh.c
@@ -9,21 +9,44 @@ IT383 - 3/14/24
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#define COMMAND_LENGTH 100
+#define MAX_HISTORY_SIZE 50
 
+//history setup 
+    char command_history [MAX_HISTORY_SIZE][COMMAND_LENGTH];
+    int history_index = 0;
+
+/*
+
+void history()
+takes no parameters
+prints the history of previously ran commands
+*/
+void history()
+{
+    for (int i = 0; i < history_index; i++) {
+        printf("%d: %s\n", (i+1),command_history[i]);
+    }
+}
 
 /*
 int run()
 takes parameters int exec_type and a character string 
 forks the process, and then uses a formatted string of arguments to call a command
 */
-int run(int exec_type, char* input_string){    
+int run(int exec_type, char* input_string){
+    //setup the initial size and index of the argument list
     int list_size = 100;
     int index = 0;
 
+    //allocate resources to the argument list and the command
     char** argument_list = malloc(list_size * sizeof(char*));
+    char* command;
     char* argument;
 
-    argument = strtok(input_string, " ");
+    //parse the input string into a command and argument list
+    command = strtok(input_string, " ");
+    argument = strtok(NULL, " ");
     while (argument != NULL)
     {
         argument_list[index] = argument;
@@ -36,32 +59,46 @@ int run(int exec_type, char* input_string){
 
         argument = strtok(NULL, " ");
     }
+    //last element needs to be null
     argument_list[index] = NULL;
+
     //fork the process
     pid_t pid;
     pid = fork();
     
     //parent process
     if(pid!= 0){
-        wait(NULL);
-        return pid;
-    }
-    
-    //child process that runs command
-    else if (pid ==0){
-        
-        if(strcmp(argument_list[0], "exit")==0)
+        int status;
+        if(waitpid(pid, &status, 0)==-1)
         {
+            perror("Error in parent process.");
             exit(0);
         }
         
+
+    }
+
+    //child process that runs command
+    else if (pid ==0){
+        //exit if the command is exit
+        if(strcmp(command, "exit")==0){
+            return 1;
+        }
+        if (strcmp(command, "history")==0)
+            {
+                history();
+            }
         //use execlp
-        if(exec_type == 0)
-        {
+        if(exec_type == 0){
+            execlp(command, argument_list[0], argument_list[1], argument_list[2], argument_list[3], argument_list[4], NULL);
+            perror("Error executing command");
             abort();
         }
         //use execvp
+
         else{
+            execvp(command, argument_list);
+            perror("Error executing command");
             abort();
         }
     }
@@ -72,7 +109,7 @@ int run(int exec_type, char* input_string){
     return 0;
 }
 
-
+//main
 int main(int argc, char* argv[]){
     //initial usage check
     if(argc > 2){
@@ -99,7 +136,6 @@ int main(int argc, char* argv[]){
         printf("**By default, execlp() will be used**");
     }
 
-
     //input setup
     char* input_string = NULL;
     size_t size = 0;
@@ -111,6 +147,17 @@ int main(int argc, char* argv[]){
         retval = getline(&input_string, &size, stdin);
         input_string[strcspn(input_string, "\r\n")] = 0;
         flag = run(exec_type, input_string);
+        if (history_index < MAX_HISTORY_SIZE) {
+            strncpy(command_history[history_index], input_string, COMMAND_LENGTH - 1);
+            command_history[history_index][COMMAND_LENGTH - 1] = '\0'; // Ensure null-termination
+            history_index++;
+        } 
+        else {
+            history_index = 0;
+            strncpy(command_history[history_index], input_string, COMMAND_LENGTH - 1);
+            command_history[history_index][COMMAND_LENGTH - 1] = '\0'; // Ensure null-termination
+            history_index++;
+        }
     } while (!flag);
 
     printf("\n** Exiting tsh  **\n");
